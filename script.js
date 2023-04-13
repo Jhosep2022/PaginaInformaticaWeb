75b17fdb0c6b80343c6eed53774251bc29d0f859
VaniaPatzi <100331206+VaniaPatzi@users.noreply.github.com>
2023-04-13 02:45:48 -0400

    script modificado

--- script.js
+++ script.js
@@ -14,3 +14,55 @@ function seleccionar(){
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
+
+window.addEventListener('load',inicializarEventos,false);
+
+function inicializarEventos()
+{
+  var ob1=document.getElementById('formulario');
+  ob1.addEventListener('submit',enviarDatos,false);
+}
+
+function enviarDatos(e)
+{
+  
+  var corr=document.getElementById('correo');
+  if (corr.value=='')
+  {
+    alert('debe ingresar los dos datos');
+    e.preventDefault();
+    return false;
+  }
+  else
+    return true;
+}
+
+
+const openModal = document.querySelector('.hero__cta');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.modal__close');
+
+openModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.add('modal--show');
+});
+
+closeModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.remove('modal--show');
+});
+
+const openModal2 = document.querySelector('.hero__cta2');
+const modal2 = document.querySelector('.modal');
+const closeModal2 = document.querySelector('.modal__close2');
+
+openModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.add('modal--show');
+});
+
+closeModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.remove('modal--show');
+});
+
